43cf07eb6bb3da0504102e59e63e00e14ccf4b99
downYotube2548 <maneeohooho3790@gmail.com>
2026-01-29 15:25:21 +0700

    improve structure of project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:attendance_system/shared/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
