a8eb67f778b0a6a1750ead4a79c310ade7564da7
downnfalls <maneeohooho3790@gmail.com>
2026-07-01 14:57:30 +0700

    chore: update API endpoint, sync Google Sign-In credentials, and optimize layout responsiveness

--- lib/core/utils/dimensions_ext.dart
+++
@@ -1,9 +0,0 @@
-import 'package:attendance_system/core/utils/responsive.dart';
-import 'package:flutter/cupertino.dart';
-
-extension ResponsiveNum on num {
-  double r(BuildContext context) {
-    final scale = Responsive.scale(context);
-    return (this * scale);
-  }
-}
